9334ac9ea6202621a4ede27aad202b6bd85df520
Markos Girgis <markos@bitloops.com>
2026-03-09 16:14:17 +0200

    Chore/add first concrete formula (#3)
    * Add initial Bitloops formula for CLI installation
    
    * Bump Bitloops version to 0.0.9
    
    * Fix SHA256 checksums for Bitloops formula on macOS and Linux
    
    * Update audit workflow to use local formula path and remove unused publish workflow
    
    * Refactor audit workflow to use local repository path for formula auditing and installation
    
    * Fix formula style issues
    
    * Update formula name due to error
    
    * Update test workflow
    
    * Add DeployMD

---
+++ Formula/bitloops.rb
@@ -0,0 +1,35 @@
+class Bitloops < Formula
+  desc "Track AI coding sessions and link them to Git commits locally"
+  homepage "https://github.com/bitloops/bitloops"
+  license "Apache-2.0"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/bitloops/bitloops/releases/download/v0.0.9/bitloops-aarch64-apple-darwin.tar.gz"
+      sha256 "33bfc5b6856adc7d370d84d859f47023ca9deb63dd8cc5070af10a8ea3b5f136"
+    end
+    on_intel do
+      url "https://github.com/bitloops/bitloops/releases/download/v0.0.9/bitloops-x86_64-apple-darwin.tar.gz"
+      sha256 "150468785b5158f9906d0144e5195bf2146271f9e298496f8891a3da098c111d"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/bitloops/bitloops/releases/download/v0.0.9/bitloops-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "095ee6571852c90d1ed769b9ad6950bc8cc9e875d0399f46110254fc16ce974b"
+    end
+    on_intel do
+      url "https://github.com/bitloops/bitloops/releases/download/v0.0.9/bitloops-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "79277b250323fc7ce99162e7f5e213969d4fe4a69e68f95e895e9302da61335a"
+    end
+  end
+
+  def install
+    bin.install "bitloops"
+  end
+
+  test do
+    system bin/"bitloops", "--version"
+  end
+end
